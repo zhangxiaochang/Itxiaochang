53a678c53b12c04fba2ed36ba2da3e74ff7780aa
zhangxc <1334241143@qq.com>
2021-01-20 22:05:38 +0800

    第一次进行idea的提交

---
+++ module/src/person.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+/**
+ * @program: workhome
+ * @description: PACKAGE_NAME
+ * @author: zhangxc
+ * @create: 2021-01-20 12:06
+ **/
+public class person implements Serializable {
+
+
+    private  int id;
+
+}
